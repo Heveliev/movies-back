1aaea62bec668218dfc4363c42020ccc06833da9
Anatolii Heveliev <tolik05.03@ukr.net>
2024-02-05 18:44:31 +0200

    added mongoDB, mongoose, connect to DB

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+const mongoose = require("mongoose");
+
 const app = require('./app');
 
-app.listen(3000, () => {console.log("server running")});
+const {DB_HOST,PORT = 3001} = process.env;
+
+
+mongoose.connect(DB_HOST)
+    .then(()=> {
+        app.listen(PORT, () => {console.log("server is running")});
+    })
+    .catch(error => {
+        console.log(error.message);
+        process.exit(1);
+    })
